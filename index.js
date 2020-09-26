98651f5d67f5e9bf857b80f127e57d20632deb4b
shahashahashaha <71528600+shahashahashaha@users.noreply.github.com>
2020-09-26 13:58:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const dateFormat = require('dateformat');
 
 client.on("guildCreate", (guild) => {
        let channel = client.channels.get(guild.channels.filter(c => c.permissionsFor(client.user).has("SEND_MESSAGES") && c.type === "text").map(r => r.id)[0])    
-       channel.send(`**قنــاتنا علـى اليوتيوب https://www.youtube.com/channel/UC_MsQabWHZKBXh4TvZenyTw**`)
+       channel.send(`**قنــاتنا علـى اليوتيوب https://www.youtube.com/channel/UCSsP_PDO-zfWwPQ_FFeZVJQ?view_as=subscriber**`)
 });
 
 
